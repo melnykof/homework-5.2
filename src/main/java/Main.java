675f6846498f2e4bed04f8f9170d53cef436e1c6
Melnykov Artem <melnykofart@gmail.com>
2020-05-26 13:46:18 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long expected = 30;
+    long actual = service.calculate(amount, registered);
+    boolean passed = expected == actual;
 
-    service.calculate(amount, registered);
+    System.out.println(passed);
   }
 }
 
